8359f6e56376b298bbb9f849d0c98586cf490051
Lumary2 <46068592+Lumary2@users.noreply.github.com>
2020-12-06 10:23:06 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -24,12 +24,39 @@ for forestString in forestList:
         if(forestString[index]== '#'):
             treeCount=treeCount+1
     except:
-        if (forestString[index] == '#'):
-            treeCount = treeCount + 1
+        break
 
     index=index+3
     lineCount=lineCount+1
     if(lineCount==323):
+        break
+
+print(treeCount)
+
+
+#Part 2 of Day 3, to concerns slope Right 1, Down 2. For the other slopes, change the indexes in the above code snippet (line 21 and 29)
+index=1
+lineCount=0
+
+for forestString in forestList:
+    lineCount=lineCount+1
+    if(lineCount<=2 or lineCount%2==0):
+        continue
+
+    for i in range(5):
+
+        forestString=forestString.rstrip('\r\n')+forestString.rstrip('\r\n')
+
+    try:
+       
+        if(forestString[index]== '#'):
+            treeCount=treeCount+1
+
+    except:
+        break
+
+    index=index+1
+    if (lineCount == 323):
         break
 
 print(treeCount)

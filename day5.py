0cc194d78f75db535c7c179dc67d094e069ffe6c
Lumary2 <b.beutlin@gmail.com>
2021-05-09 13:44:42 +0200

    test

--- day5.py
+++ day5.py
@@ -19,7 +19,7 @@ for ticket in file:
     #loop through chars of ticket
     countRow=0
     countCol=0
-    for element in ticket2:
+    for element in tikket2:
 
         if(rangeHighRow==rangeLowRow):
             row=rangeHighRow
